a8ac584e7caeedb749054c829c99dc9572cab959
Jason Goecke <jsgoecke@mayluc.local>
2008-12-18 02:22:00 -0800

    Commiting changes to scoped variables

--- hammer.rb
+++ hammer.rb
@@ -1,5 +1,7 @@
 require 'uuidtools'
 
+#Intialize the component attaching to the database if configured
+#and creating the Hammer object
 initialization do
   begin
     #Now launch the hammer and let it run
